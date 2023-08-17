485c7fcbb84bd1927a5833199b5b1c61d0c7985c
ustat <ustat.parjasp@gmail.com>
2023-08-16 22:19:11 -0400

    email commit

---
+++ just_emailer.py
@@ -0,0 +1,25 @@
+import smtplib
+from email.message import EmailMessage
+
+
+def send_email(subject, body, to_email):
+    # Your email credentials
+    email_address = 'sohampy0@gmail.com'
+    email_password = 'macazhlhbkcipwxx'
+
+    # Create the email message
+    msg = EmailMessage()
+    msg.set_content(body)
+    msg['Subject'] = subject
+    msg['From'] = email_address
+    msg['To'] = to_email
+
+    # Connect to Gmail's SMTP server
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+        smtp.login(email_address, email_password)
+        smtp.send_message(msg)
+
+
+send_email('testing not from raspberry', 'some one is here, SOS',
+           'soham.parjasp@gmail.com,ustat.parjasp@gmail.com')
+print('email sent')
---
+++ raspberry_mailer.py
@@ -0,0 +1,29 @@
+import smtplib
+from email.message import EmailMessage
+from gpiozero import Button
+
+
+def send_email(subject, body, to_email):
+    # Your email credentials
+    email_address = 'sohampy0@gmail.com'
+    email_password = 'macazhlhbkcipwxx'
+
+    # Create the email message
+    msg = EmailMessage()
+    msg.set_content(body)
+    msg['Subject'] = subject
+    msg['From'] = email_address
+    msg['To'] = to_email
+
+    # Connect to Gmail's SMTP server
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+        smtp.login(email_address, email_password)
+        smtp.send_message(msg)
+
+
+button = Button(2)
+button.wait_for_press()
+print('button pressed')
+send_email('testing from raspberry with 2', 'some one is here, SOS',
+           'soham.parjasp@gmail.com,ustat.parjasp@gmail.com')
+print('email sent')
